be4de4f084b3a083488c192801e4e4743c7b189b
GOREMbIKA <Cyrreal@yandex.ru>
2023-03-05 16:44:09 +0300

    solution to pass test

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,7 +1,12 @@
 function makeDiagonalRed(table) {
-  const td = document.getElementsByTagName('td');
-  const step = table.rows.length + 1;
-  for (let i = 0; i <= td.length - 1; i += step) {
-    td[i].style.backgroundColor = 'red'
+  // const td = document.getElementsByTagName('td');
+  // const step = table.rows.length + 1;
+  // for (let i = 0; i <= td.length - 1; i += step) {
+  //   td[i].style.backgroundColor = "red"
+  // } //мое изначальное решение
+
+  for (let i = 0; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    row.cells[i].style.backgroundColor = 'red';
   }
 }
